c2ac45e862d483f97340613a178cd4262f16d455
rayyanalk <70042896+rayyanalk@users.noreply.github.com>
2023-08-31 19:40:51 -0700

    Added database files for statistical manipulation

---
+++ home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6af72dc784.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@500&family=MonteCarlo&family=WindSong:wght@500&display=swap"
+        rel="stylesheet">
+    <title>Soulsborne Compendium</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <script src="jquery.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg" style="background-color: black">
+        <a class="navbar-brand" href="home.php" id="home">
+            <i class="fas fa-adjust"></i>
+        </a>
+        <button class="navbar-toggler custom-togglerH" type="button" data-toggle="collapse" data-target="#navbarToggler"
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-iconH">
+                <i class="fas fa-bars" style="color:#;"></i>
+            </span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="challenge.php" style="color:red;">Challenge</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gear_display.php" id="home">Gear List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add_gear.php" id="home">Add Gear</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <h1 class="col-12 mt-4 mb-4" id="titleH">Soulsborne Compendium</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6" id="bbutton">
+                <a href="bloodboss.php" class="btn btn-secondary text-nowrap btn-lg btn-block mt-6 mt-md-4"
+                    role="button"><img src="bloodborne title.png" width=100% height=100% /></a>
+            </div>
+            <div class="col-12 col-md-6" id="dbutton">
+                <a href="soulsboss.php" class="btn btn-secondary text-nowrap btn-lg btn-block mt-6 mt-md-4"
+                    role="button"><img src="dark souls title.png" width=100% height=100% /></a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6" id="bbquote">
+                <p>" Seek the old blood. Let us partake in communion,
+                    and feast upon the old blood. Our thirst for blood satiates us, soothes our fears. But beware the
+                    frailty of men. Their wills are weak, minds young. The foul beasts will lure the meek into the
+                    depths. Were it not for fear, death would go unlamented. "</p>
+            </div>
+            <div class="col-12 col-md-6" id="dsquote">
+                <p>" Fire came to be, and with it disparity. Heat and cold, life and death, light and dark.
+                    Dark was seen as a curse. Shadow is not cast, but born of fire.
+                    And the brighter the flame, the deeper the shadow.
+                    Inherit fire, and harness the dark. Such is the calling of a true leader. "</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
